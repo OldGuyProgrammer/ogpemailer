510cbc3a92bd620e4ad0e0e63932e899f98ea8f4
Jim Olivi <jim@olivi.us>
2023-10-30 10:05:56 -0400

    V 0.1.2 fix read of private key.

--- components/firebase.js
+++ components/firebase.js
@@ -23,7 +23,8 @@ import serviceAccount from "../firebase_auth.json" assert { type: "json" };
 var db;
 export function initializeFirebase() {
   serviceAccount.private_key_id = process.env.PRIVATE_KEY_ID;
-  serviceAccount.private_key = process.env.PRIVATE_KEY;
+  serviceAccount.private_key = process.env.PRIVATE_KEY.replace(/\\n/g, "\n");
+  // console.log(serviceAccount.private_key);
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
